b3451b23a3673edf503fa9f12af561c30fa67857
tagaretiro <63424036+tagaretiro@users.noreply.github.com>
2020-06-16 21:05:21 -0500

    Add files via upload

--- install.py
+++ install.py
@@ -10,15 +10,15 @@ def download_url(url, save_path, chunk_size=128):
             fd.write(chunk)
 
 def extract_7z_job():
-    process_7z = sub.Popen(['7za.exe', 'e', 'ansible.zip', '-oTEMP', '-y'])
+    process_7z = sub.Popen(['7za.exe', 'x', 'ansible.zip', '-oTEMP', '-y'])
     process_7z.communicate()
 
 def run_install():
-    os.chdir('TEMP')
+    os.chdir('TEMP\\ansible-2.9.2')
     process_7z = sub.Popen(['python.exe', 'setup.py', 'install'])
     process_7z.communicate()
 
 download_url('https://github.com/ansible/ansible/archive/v2.9.2.zip', 'ansible.zip')
 extract_7z_job()
-shutil.copy2('template\\setup.py', 'TEMP')
+shutil.copy2('template\\setup.py', 'TEMP\\ansible-2.9.2')
 run_install()
